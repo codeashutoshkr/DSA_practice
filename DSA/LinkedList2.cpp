085328c273bc6eb5d2447e348375d276d731eb42
Ashutosh Kumar <emailashutoshkr@gmail.com>
2025-03-27 22:56:32 +0530

    adding LL2 and OpenTheDragonScroll

--- DSA/LinkedList2.cpp
+++ DSA/LinkedList2.cpp
@@ -13,6 +13,15 @@ class Node{
         this -> prev = NULL;
         this -> next = NULL;
     }
+    // destructor
+    ~Node(){
+        int val = this -> data;
+        if(next != NULL){
+            delete next;
+            next = NULL;
+        }
+        cout<<"Memory free for Node with data "<<val<<endl;
+    }
 };
 // traversing a LL
    void print(Node* head){
@@ -34,30 +43,102 @@ class Node{
     return len;
    }
    // insert at head
-   void insertAtHead(Node* &head, int d){
-    Node* temp = new Node(d);
-    temp -> next = head;
-    head -> prev = temp;
-    head = temp;
+   void insertAtHead(Node* &tail, Node* &head, int d){
+    // empty list
+    if(head == NULL){
+        Node* temp = new Node(d);
+        head = temp;
+        tail = temp;
+    }
+    else{
+        Node* temp = new Node(d);
+        temp -> next = head;
+        head -> prev = temp;
+        head = temp;
+    }
+   
    }
    // insert at tail
-   void insertAtTail(Node* &tail, int d){
-    Node* temp = new Node(d);
-    tail -> next = temp;
-    temp -> prev = tail;
-    tail = temp;
+   void insertAtTail(Node* &tail,Node* &head, int d){
+    if(tail == NULL){
+       Node* temp = new Node(d);
+       tail = temp;
+       head = temp;
+    }
+    else{
+        Node* temp = new Node(d);
+        tail -> next = temp;
+        temp -> prev = tail;
+        tail = temp;
+    }
    }
+        // insert at a position
+        void insertAtPosition(Node* &tail, Node* &head, int position, int d){
+            // insert at head
+            if(position == 1){
+              insertAtHead(head, tail, d);
+              return;
+            }
+    
+            Node* temp = head;
+            int cnt = 1;
+            while(cnt < position-1){
+              temp = temp->next;
+              cnt++;
+            }
+            // insert at last position
+            if(temp -> next == NULL){
+              insertAtTail(tail, head, d);
+              return;
+            }
+            // creating  a node for d
+            Node* nodeToInsert = new Node(d);
+            nodeToInsert -> next = temp -> next;
+            temp -> next -> prev = nodeToInsert;
+            temp -> next = nodeToInsert;
+            nodeToInsert -> prev = temp;
+          }
+          void deleteNode(int position, Node* &head){
+            // delete first or start node
+            if(position == 1){
+                Node* temp = head;
+                temp -> next -> prev = NULL;
+                head = temp -> next;
+                temp -> next = NULL;
+                delete temp;
+            }
+            else{
+                // delete any middle or last node
+                Node* curr = head;
+                Node* prev = NULL;
+                int cnt = 1;
+                while(cnt < position){
+                    prev = curr;
+                    curr = curr -> next;
+                    cnt++;
+                }
+                curr -> prev = NULL;
+                prev -> next = curr -> next;
+                curr -> next = NULL;
+                delete curr;
+            }
+          }
 
    int main(){
     Node* node1 = new Node(10);
-    Node* head = node1;
+    Node* head = NULL;
+    Node* tail = NULL;
     print(head);
     cout<<getLength(head)<<endl;
-    insertAtHead(head, 11);
+    insertAtHead(tail, head, 11);
     print(head);
-    insertAtHead(head, 15);
+    insertAtHead(tail, head, 15);
     print(head);
-    insertAtTail(head, 25);
+    insertAtTail(tail, head, 25);
+    print(head);
+    insertAtPosition(tail, head, 2, 100);
+    print(head);
+    deleteNode(1, head);
     print(head);
     
    }
